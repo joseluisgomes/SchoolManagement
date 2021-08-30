a4b6df666583a5961aeb49ec9349be1d53d63046
joseluisgomes <joslus.luis@gmail.com>
2021-08-30 23:29:08 +0100

    Antmatchers according to the user's authorities. Implemented 1 method on ApplicationUserRole class

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -25,17 +27,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() // TODO: After testing the API -> delete this line
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-                .antMatchers("/api/**").hasAnyRole(
-                        STUDENT.name(),
-                        ADMIN.name(),
-                        ADMIN_TRAINEE.name()
-                )
+                .antMatchers("/api/**").hasAnyRole(STUDENT.name(), ADMIN.name(), ADMIN_TRAINEE.name())
+                    .antMatchers(HttpMethod.DELETE, "/v1/**").hasAuthority(COURSE_WRITE.name())
+                    .antMatchers(HttpMethod.POST, "/v1/**").hasAuthority(COURSE_WRITE.name())
+                    .antMatchers(HttpMethod.PUT, "/v1/**").hasAuthority(COURSE_WRITE.name())
+                    .antMatchers(HttpMethod.GET, "/v1/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,24 +47,31 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails annaSmith = User.builder()
+        UserDetails bobDylan = User.builder()
                 .username("bobDylan")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
+    //            .roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails tomHolland = User.builder()
                 .username("tomHolland")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+    //            .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails michaelJordan = User.builder()
                 .username("michaelJordan")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN_TRAINEE.name()) // ROLE_ADMIN_TRAINEE
+    //            .roles(ADMIN_TRAINEE.name()) // ROLE_ADMIN_TRAINEE
+                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
 
-        return new InMemoryUserDetailsManager(annaSmith, tomHolland, michaelJordan);
+        return new InMemoryUserDetailsManager(
+                bobDylan,
+                tomHolland,
+                michaelJordan
+        );
     }
 }
