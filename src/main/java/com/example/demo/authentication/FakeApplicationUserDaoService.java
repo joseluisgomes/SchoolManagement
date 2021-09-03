c0cefbee0e696aaa0890bd595f6e1ef1413a7be6
joseluisgomes <joslus.luis@gmail.com>
2021-09-03 12:30:37 +0100

    Implemented the ApplicationUserDao interface.

---
+++ src/main/java/com/example/demo/authentication/FakeApplicationUserDaoService.java
@@ -0,0 +1,61 @@
+package com.example.demo.authentication;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.demo.security.ApplicationUserRole.*;
+
+@Repository("fake") // fake = Repository name
+public class FakeApplicationUserDaoService implements ApplicationUserDao {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers().stream()
+                .filter(applicationUser -> applicationUser.getUsername().equals(username))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        return Lists.newArrayList(
+                new ApplicationUser(
+                        STUDENT.getGrantedAuthorities(),
+                        "bobDylan",
+                        passwordEncoder.encode("password"),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        ADMIN.getGrantedAuthorities(),
+                        "tomHolland",
+                        passwordEncoder.encode("password123"),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        ADMIN_TRAINEE.getGrantedAuthorities(),
+                        "michaelJordan",
+                        passwordEncoder.encode("password123"),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
+}
